018e98a41e994cc5d9747295801f3413f6c2f109
Bodrul813 <mdbodrulislam813@gmail.com>
2025-07-09 03:28:01 +0600

    Uploaed Architectuer of EMS

---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Globalization;
+
+namespace BaseLibrary.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string? CivilId { get; set; }
+        public string? FileNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? JobName { get; set; }
+        public string? Address { get; set; }
+        public string? TelPhoneNumber { get; set; }
+        public string? Photo {  get; set; }
+        public string? Other {  get; set; }
+
+    }
+}
